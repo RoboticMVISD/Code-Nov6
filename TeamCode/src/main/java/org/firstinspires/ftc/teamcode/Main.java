e8345d3d8dbaaf351e783a75f24e9e84711ebd0d
BENAVIDES <015167@mvisd.com>
2025-11-06 16:13:59 -0600

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+
+@TeleOp (name = "Teleop 2025")
+public class Main extends OpMode {
+
+   MovementSystem movementSystem = new MovementSystem();
+   Intake intake = new Intake();
+   Shooter shooter = new Shooter();
+
+
+   public void init()
+    {
+        intake.init(this);
+        movementSystem.init(this);
+        shooter.init(this);
+    }
+
+    @Override
+    public void loop()
+    {
+        intake.loop();
+        movementSystem.loop();
+        shooter.loop();
+    }
+}
